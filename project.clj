5f97689aed4dc81a236c6665613e7b448e71d155
flosell <florian.sellmayr@gmail.com>
2015-03-07 18:29:44 +0100

    fix license in project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject lambdacd-cctray "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "Apache License, version 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
   :deploy-repositories [["clojars" {:creds :gpg}]]
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [lambdacd "0.1.0-alpha12"]
