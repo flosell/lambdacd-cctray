0cde3c536feb4d62b990d101f06212dcc8a36add
Florian Sellmayr <florian.sellmayr@gmail.com>
2015-09-07 10:23:42 +0200

    Version 0.3.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lambdacd-cctray "0.3.1-SNAPSHOT"
+(defproject lambdacd-cctray "0.3.1"
   :description "cctray support for lambdacd"
   :url "http://github.com/flosell/lambdacd-cctray"
   :license {:name "Apache License, version 2.0"
