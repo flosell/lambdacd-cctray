546c9bcc426577ad25ee310af3432cbe3203bc6c
Florian Sellmayr <florian.sellmayr@gmail.com>
2018-04-23 22:12:27 +0700

    Run tests against multiple versions of lambdacd and clojure to test compatibility issues

--- project.clj
+++ project.clj
@@ -1,3 +1,11 @@
+(def lambdacd-version (or
+                        (System/getenv "LAMBDACD_VERSION")
+                        "0.13.5"))
+
+(def clojure-version-to-use (or
+                              (System/getenv "CLOJURE_VERSION")
+                              "1.7.0"))
+
 (defproject lambdacd-cctray "0.5.0-SNAPSHOT"
   :description "cctray support for lambdacd"
   :url "http://github.com/flosell/lambdacd-cctray"
@@ -5,8 +13,8 @@
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
   :deploy-repositories [["clojars" {:creds :gpg}]
                         ["releases" :clojars]]
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [lambdacd "0.13.5"]
+  :dependencies [[org.clojure/clojure ~clojure-version-to-use]
+                 [lambdacd ~lambdacd-version]
                  [clj-time "0.9.0"]
                  [org.clojure/data.xml "0.0.8"]]
   :profiles {:dev {:dependencies [[clj-cctray "0.10.0"]
